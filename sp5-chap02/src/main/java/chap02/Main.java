2f2cfed800b058e8fa7cb5eda502c1bc06a31abb
hiwon-lee <leather2790@naver.com>
2023-10-31 17:33:07 +0900

    [feat] chap02+03 재설정

---
+++ sp5-chap02/src/main/java/chap02/Main.java
@@ -0,0 +1,18 @@
+package chap02;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnnotationConfigApplicationContext ctx = 
+				new AnnotationConfigApplicationContext(AppContext.class);
+		Greeter g = ctx.getBean("greeter", Greeter.class);
+		String msg = g.greet("스프링");
+		System.out.println(msg);
+		ctx.close();
+
+	}
+
+}
